d5941c0388925b80ad15866c8932060fdee3dd55
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-05-01 15:54:45 +0700

    Menambahkan HewanRepository untuk mengambil data hewan dari API

---
+++ lib/data/repositories/hewan_repository.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/hewan_model.dart';
+import '../providers/storage_provider.dart';
+
+class HewanRepository {
+  final String baseUrl = "https://ternak-be-production.up.railway.app/api/v1";
+  final StorageProvider storage = StorageProvider();
+
+  Future<List<HewanModel>> getAllHewan() async {
+    final token = await storage.getToken();
+
+    final response = await http.get(
+      Uri.parse('$baseUrl/hewan'),
+      headers: {'Authorization': 'Bearer $token', 'Accept': 'application/json'},
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> body = jsonDecode(response.body);
+      final List<dynamic> data = body['data'];
+      return data.map((item) => HewanModel.fromJson(item)).toList();
+    } else {
+      throw Exception("Gagal mengambil data hewan");
+    }
+  }
