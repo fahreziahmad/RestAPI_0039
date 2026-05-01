1107ef4c6c3efc18da447b3f8deea4ef74677520
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-05-01 15:59:57 +0700

    Menambahkan AuthBloc untuk manajemen autentikasi (AppStarted & Login)

---
+++ lib/logic/bloc/auth/auth_bloc.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'auth_event.dart';
+import 'auth_state.dart';
+import '../../../data/repositories/auth_repository.dart';
+import 'dart:developer' as developer;
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final AuthRepository repository;
+
+  AuthBloc({required this.repository}) : super(AuthInitial()) {
+    
+    on<AppStarted>((event, emit) async {
+      final token = await repository.getToken();
+      if (token != null) {
+        emit(Authenticated(token));
+      } else {
+        emit(Unauthenticated());
+      }
+    });
+
+    on<LoginRequested>((event, emit) async {
+      emit(AuthLoading());
+      developer.log('Attempting login for: ${event.email}', name: 'AuthBloc');
+
+      try {
+        await repository.login(event.email, event.password);
+
+        final token = await repository.getToken();
+
+        if (token != null) {
+          emit(Authenticated(token));
+          developer.log('✅ Status: Authenticated', name: 'AuthBloc');
+        } else {
+          throw 'Token tidak ditemukan setelah login';
+        }
+      } catch (e) {
+        emit(AuthError(e.toString()));
+        developer.log('❌ Status: AuthError - $e', name: 'AuthBloc');
+      }
+    });
