5f831d4ab2b82aa64f58d6d117f6260b9bb5b764
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-05-01 15:51:44 +0700

    Menambahkan AuthRepository untuk autentikasi dan manajemen token

---
+++ lib/data/repositories/auth_repository.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import '../models/user_model.dart';
+import 'dart:developer' as developer;
+
+class AuthRepository {
+  final String baseUrl = "https://ternak-be-production.up.railway.app/api/v1";
+  final _storage = const FlutterSecureStorage();
+
+  Future<void> persistToken(String token) async {
+    await _storage.write(key: 'jwt_token', value: token);
+  }
+
+  Future<String?> getToken() async {
+    return await _storage.read(key: 'jwt_token');
+  }
+
+  Future<void> deleteToken() async {
+    await _storage.delete(key: 'jwt_token');
+  }
+
+  Future<UserModel> login(String email, String password) async {
+    try {
+      final response = await http.post(
+        Uri.parse('$baseUrl/auth/login'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: jsonEncode({'email': email, 'password': password}),
+      );
