70b6586f4b98b0bc3b229bfa6039d7b7a8fcfdef
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-05-01 18:02:30 +0700

    menambahkan reusable glassmorphism TextFormField component

--- lib/ui/pages/register_page.dart
+++ lib/ui/pages/register_page.dart
@@ -219,7 +219,47 @@ class _RegisterPageState extends State<RegisterPage> {
         ),
       ),
     ),
-  ),
-)
+  );
+),
+
+Widget _buildGlassTextField({
+  required TextEditingController controller,
+  required String hint,
+  required IconData icon,
+  bool isPassword = false,
+  Widget? suffixIcon,
+  TextInputType? keyboardType,
+  String? Function(String?)? validator,
+}) {
+  return Container(
+    decoration: BoxDecoration(
+      color: Colors.white.withOpacity(0.2),
+      borderRadius: BorderRadius.circular(15),
+    ),
+    child: TextFormField(
+      controller: controller,
+      obscureText: isPassword,
+      keyboardType: keyboardType,
+      validator: validator,
+      style: const TextStyle(color: Colors.white),
+      decoration: InputDecoration(
+        hintText: hint,
+        hintStyle: const TextStyle(color: Colors.white60),
+        prefixIcon: Icon(icon, color: Colors.white70),
+        suffixIcon: suffixIcon,
+        border: InputBorder.none,
+        errorStyle: const TextStyle(
+          color: Colors.yellowAccent,
+          fontWeight: FontWeight.bold,
+        ),
+        contentPadding: const EdgeInsets.symmetric(
+          vertical: 15,
+          horizontal: 20,
+        ),
+      ),
+    ),
+  );
+}
+
 
   
