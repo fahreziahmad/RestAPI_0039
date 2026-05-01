5016f1135732ea96dba15b2feca34c24cdb220d1
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-05-01 16:06:02 +0700

    Menambahkan HewanBloc untuk pengelolaan state data hewan

---
+++ lib/logic/bloc/hewan/hewan_bloc.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:restapi/logic/bloc/hewan/hewan_event.dart';
+import 'package:restapi/logic/bloc/hewan/hewan_state.dart';
+import '../../../data/repositories/hewan_repository.dart';
+
+class HewanBloc extends Bloc<HewanEvent, HewanState> {
+  final HewanRepository repository;
+
+  HewanBloc({required this.repository}) : super(HewanInitial()) {
+    on<FetchHewan>((event, emit) async {
+      emit(HewanLoading());
+      try {
+        final list = await repository.getAllHewan();
+        emit(HewanLoaded(list));
+      } catch (e) {
+        emit(HewanError(e.toString()));
+      }
+    });
